274c43a82d3d4d2600df4536203165436643baec
Alec Savvy <alecjsavvy@gmail.com>
2023-03-23 07:19:34 -0600

    use seaography generator

--- src/db.rs
+++
@@ -1,8 +0,0 @@
-use sea_orm::*;
-
-const DATABASE_URL: &'static str = "postgres://postgres:pass@localhost:5432/default_db";
-
-pub async fn set_up_db() -> Result<DatabaseConnection, DbErr> {
-    let db = Database::connect(DATABASE_URL).await?;
-    Ok(db)
-}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,68 @@
-use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
-use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::{
-    extract::Extension,
-    response::{self, IntoResponse},
-    routing::get,
-    Router, Server,
+use async_graphql::{
+    dataloader::DataLoader,
+    http::{playground_source, GraphQLPlaygroundConfig},
+    EmptyMutation, EmptySubscription, Schema,
 };
-use query::AudiusSchema;
+use async_graphql_poem::GraphQL;
+use dotenv::dotenv;
+use lazy_static::lazy_static;
+use poem::{get, handler, listener::TcpListener, web::Html, IntoResponse, Route, Server};
+use raudius_graph::*;
+use sea_orm::Database;
+use std::env;
 
-use crate::query::QueryRoot;
-
-pub mod db;
-pub mod entities;
-pub mod query;
-
-pub type AppResult<T = ()> = Result<T, Box<dyn std::error::Error>>;
-
-async fn graphql_handler(schema: Extension<AudiusSchema>, req: GraphQLRequest) -> GraphQLResponse {
-    schema.execute(req.into_inner()).await.into()
+lazy_static! {
+    static ref URL: String = env::var("URL").unwrap_or("0.0.0.0:8000".into());
+    static ref ENDPOINT: String = env::var("ENDPOINT").unwrap_or("/".into());
+    static ref DATABASE_URL: String =
+        env::var("DATABASE_URL").expect("DATABASE_URL environment variable not set");
+    static ref DEPTH_LIMIT: Option<usize> = env::var("DEPTH_LIMIT").map_or(None, |data| Some(
+        data.parse().expect("DEPTH_LIMIT is not a number")
+    ));
+    static ref COMPLEXITY_LIMIT: Option<usize> = env::var("COMPLEXITY_LIMIT")
+        .map_or(None, |data| {
+            Some(data.parse().expect("COMPLEXITY_LIMIT is not a number"))
+        });
 }
 
-async fn graphiql() -> impl IntoResponse {
-    response::Html(GraphiQLSource::build().endpoint("/").finish())
+#[handler]
+async fn graphql_playground() -> impl IntoResponse {
+    Html(playground_source(GraphQLPlaygroundConfig::new(&*ENDPOINT)))
 }
 
 #[tokio::main]
-async fn main() -> AppResult {
-    let db = db::set_up_db().await?;
-    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
-        .data(db)
-        .finish();
-
-    let app = Router::new()
-        .route("/", get(graphiql).post(graphql_handler))
-        .layer(Extension(schema));
-
-    println!("GraphiQL IDE: http://localhost:8000");
-
-    Server::bind(&"127.0.0.1:8000".parse().unwrap())
-        .serve(app.into_make_service())
+async fn main() {
+    dotenv().ok();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_test_writer()
+        .init();
+    let database = Database::connect(&*DATABASE_URL)
         .await
-        .unwrap();
-    Ok(())
+        .expect("Fail to initialize database connection");
+    let orm_dataloader: DataLoader<OrmDataloader> = DataLoader::new(
+        OrmDataloader {
+            db: database.clone(),
+        },
+        tokio::spawn,
+    );
+    let mut schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(database)
+        .data(orm_dataloader);
+    if let Some(depth) = *DEPTH_LIMIT {
+        schema = schema.limit_depth(depth);
+    }
+    if let Some(complexity) = *COMPLEXITY_LIMIT {
+        schema = schema.limit_complexity(complexity);
+    }
+    let schema = schema.finish();
+    let app = Route::new().at(
+        &*ENDPOINT,
+        get(graphql_playground).post(GraphQL::new(schema)),
+    );
+    println!("Visit GraphQL Playground at http://{}", *URL);
+    Server::new(TcpListener::bind(&*URL))
+        .run(app)
+        .await
+        .expect("Fail to start web server");
 }
