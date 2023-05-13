da55563e423c6a0472d44f5ff1d06673033fd844
Alec Savvy <alecjsavvy@gmail.com>
2023-05-12 21:32:15 -0600

    handwrite

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,40 @@
+use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
+
 pub mod entities;
 pub mod queries;
 pub mod subscriptions;
 
+use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
+use axum::{
+    response::{self, IntoResponse},
+    routing::get,
+    Extension, Router, Server,
+};
+use queries::RootQuery;
+
+async fn graphql_handler(
+    schema: Extension<Schema<RootQuery, EmptyMutation, EmptySubscription>>,
+    req: GraphQLRequest,
+) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn graphiql() -> impl IntoResponse {
+    response::Html(GraphiQLSource::build().endpoint("/").finish())
+}
+
 #[tokio::main]
 async fn main() {
-    println!("hai")
+    let query = RootQuery::default();
+    let schema = Schema::build(query, EmptyMutation, EmptySubscription).finish();
+
+    let app = Router::new()
+        .route("/", get(graphiql).post(graphql_handler))
+        .layer(Extension(schema));
+    println!("GraphiQL IDE: http://localhost:8000");
+
+    Server::bind(&"127.0.0.1:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
--- src/queries/mod.rs
+++ src/queries/mod.rs
@@ -2,3 +2,9 @@ pub mod playlists;
 pub mod tips;
 pub mod tracks;
 pub mod users;
+
+use async_graphql::MergedObject;
+pub use users::*;
+
+#[derive(Debug, MergedObject, Default)]
+pub struct RootQuery(UserQuery);
